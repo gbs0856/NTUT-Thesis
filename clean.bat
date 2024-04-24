ef6216734477d8d4d6bab56ee1a079e2f029ebac
Chuan-Chi Lai <160428276+gbs0856@users.noreply.github.com>
2024-04-25 01:55:41 +0800

    Add files via upload

---
+++ clean.bat
@@ -0,0 +1,7 @@
+del *.aux
+del *.dvi
+del *.lof
+del *.log
+del *.toc
+del *.blg
+del *.gz
---
+++ make.bat
@@ -0,0 +1,6 @@
+xelatex.exe -synctex=1 -interaction=nonstopmode "Dissertation".tex
+bibtex.exe Dissertation
+xelatex.exe -synctex=1 -interaction=nonstopmode "Dissertation".tex
+xelatex.exe -synctex=1 -interaction=nonstopmode "Dissertation".tex
+pause
+
